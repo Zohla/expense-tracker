96c5d10eb2ebc790f70211affdb21c0ab5c25eec
Maren Lilleberre <marenlilleberre@gmail.com>
2022-05-11 18:21:18 +0200

    make header and footer

---
+++ src/components/UI/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <ul>
+        <li className="footer-list-item">
+          <a href="mailto:marenlilleberre@gmail.com">Contact</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/UI/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="h1">Expence Tracker</h1>
+    </div>
+  );
+}
+
+export default Header;
